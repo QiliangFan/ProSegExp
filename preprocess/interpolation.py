34acc0b18554604afc92b0a1ca3a0f3eb2418ae1
qiliang fan <1035847702@qq.com>
2022-06-19 10:03:38 +0800

    update

---
+++ preprocess/interpolation.py
@@ -0,0 +1,18 @@
+"""
+展示原始数据的尺寸分布，来说明差值的目的尺寸和差值方式的区别
+"""
+
+import SimpleITK as sitk
+import numpy as np
+import os
+from glob import glob
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    train_root = "d:/TrainData"
+
+    main()
